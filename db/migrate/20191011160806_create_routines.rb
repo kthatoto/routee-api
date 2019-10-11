98ac614ddca3ffb191706869e58ebca0cd13476c
kthatoto <kthatoto@gmail.com>
2019-10-12 01:10:30 +0900

    Add routines

---
+++ db/migrate/20191011160806_create_routines.rb
@@ -0,0 +1,11 @@
+class CreateRoutines < ActiveRecord::Migration[5.2]
+  def change
+    create_table :routines do |t|
+      t.integer :user_id,             null: false
+      t.integer :routine_template_id, null: false
+      t.boolean :achieved,            null: false, default: false
+      t.integer :count
+      t.timestamps
+    end
+  end
+end
