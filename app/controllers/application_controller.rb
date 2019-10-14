1a87257eb35767d2cc6b4fd530ba6e9af71aad51
kthatoto <kthatoto@gmail.com>
2019-10-14 22:17:14 +0900

    User create

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::API
   def authenticate
     token = request.headers["Authorization"]
     digested_token = Digest::SHA1.hexdigest(token)
-    user = User.find_by(token: digested_token)
+    user = User.find_by(token_digest: digested_token)
     @current_user = user if user
   end
 end
