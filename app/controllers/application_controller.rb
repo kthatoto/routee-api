d92924ad83efadc6c7a81147a2e79f2eac2aaf6a
kthatoto <kthatoto@gmail.com>
2019-10-14 16:38:36 +0900

    Add set user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate
+
+  def authenticate
+    token = request.headers["Authorization"]
+    digested_token = Digest::SHA1.hexdigest(token)
+    user = User.find_by(token: digested_token)
+    @current_user = user if user
+  end
 end
