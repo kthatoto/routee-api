3439ef9ef0aee261791e36b68f78f83f6b7cd28e
kthatoto <kthatoto@gmail.com>
2019-10-18 16:11:13 +0900

    Add column to routine template

--- db/migrate/20191011160312_create_routine_templates.rb
+++ db/migrate/20191011160312_create_routine_templates.rb
@@ -3,6 +3,7 @@ class CreateRoutineTemplates < ActiveRecord::Migration[5.2]
     create_table :routine_templates do |t|
       t.integer :user_id, null: false
       t.string :name,     null: false
+      t.date :start_date
       t.text :description
       t.integer :target_count
       t.timestamps
