4f4f94321a87d0d924deac3035c25b545f30be79
kthatoto <kthatoto@gmail.com>
2019-10-21 04:11:36 +0900

    Add default value

--- db/migrate/20191011160312_create_routine_templates.rb
+++ db/migrate/20191011160312_create_routine_templates.rb
@@ -1,12 +1,12 @@
 class CreateRoutineTemplates < ActiveRecord::Migration[5.2]
   def change
     create_table :routine_templates do |t|
-      t.integer :user_id, null: false
-      t.string :name,     null: false
+      t.integer :user_id,  null: false
+      t.string :name,      null: false
+      t.boolean :archived, null: false, default: false
       t.date :start_date
       t.text :description
       t.integer :target_count
-      t.boolean :archived
       t.timestamps
     end
   end
