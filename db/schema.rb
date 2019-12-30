50eaa8c05c6d44c508402dd4706df9e564abe3d6
kthatoto <kthatoto@gmail.com>
2019-12-30 16:37:02 +0900

    Migrate

--- db/schema.rb
+++ db/schema.rb
@@ -2,17 +2,17 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_11_160806) do
+ActiveRecord::Schema.define(version: 2019_12_29_191406) do
 
-  create_table "routine_templates", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "routine_templates", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "name", null: false
     t.integer "interval_type", null: false
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 2019_10_11_160806) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "routine_terms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "routine_terms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "interval_type", null: false
     t.date "start_date", null: false
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 2019_10_11_160806) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "routines", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "routines", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "routine_template_id", null: false
     t.integer "routine_term_id", null: false
@@ -43,12 +43,11 @@ ActiveRecord::Schema.define(version: 2019_10_11_160806) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "account_id", null: false
-    t.string "password_digest", null: false
-    t.string "token_digest"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
