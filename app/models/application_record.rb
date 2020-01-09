0ce1d3a2c5374af5d0a9a9722fff0c98434d5353
kthatoto <kthatoto@gmail.com>
2020-01-09 18:14:35 +0900

    Override insert_all!

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,16 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  # @param Array of Hash<Symbol => Any>
+  def self.insert_all!(records)
+    now = Time.current
+    processed_records = records.map do |r|
+      self.defined_enums.each do |column_name, hash|
+        next unless r[column_name.to_sym]
+        r[column_name.to_sym] = hash[r[column_name.to_sym]]
+      end
+      r.merge(created_at: now, updated_at: now)
+    end
+    super(processed_records)
+  end
 end
